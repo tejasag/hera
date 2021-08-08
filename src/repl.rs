d1482428583f671e906d7cb6ef7e832a2b6bfc1c
Will Lane <williamlane923@gmail.com>
2021-08-08 23:00:28 +0200

    Moved functions into associated positions, removed uneeded borrowing, ignored result type, cleaned up imports

--- src/repl.rs
+++ src/repl.rs
@@ -1,24 +1,29 @@
-use super::token::Token;
+use crate::{lexer::Lexer, token::Token};
 use std::io::{stdin, stdout, Write};
 
 pub fn start() {
     loop {
         print!(">> ");
-        stdout().flush();
+
+        let _ = stdout().flush();
+
         let mut input_string = String::new();
         stdin()
             .read_line(&mut input_string)
             .ok()
             .expect("Could not take in a string!");
+
         if input_string.is_empty() {
             println!("\nPlease enter valid code.");
             continue;
         }
-        let mut l = super::lexer::New(input_string);
-        let mut tok = l.next_token();
-        while tok != Token::EOF {
-            println!("Token: {:?}", tok);
-            tok = l.next_token();
+        let mut lexer = Lexer::new(input_string);
+        let mut token = lexer.next_token();
+
+        while token != Token::EOF {
+            println!("Token: {:?}", token);
+
+            token = lexer.next_token();
         }
     }
 }
